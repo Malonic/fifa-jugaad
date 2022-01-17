65f039092717bec3f37373a776d6a70fa2c01b95
Aditya <adityabanka08@gmail.com>
2022-01-17 22:04:25 +0530

    1st update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Setup Python ----------------------------------------------- #
-import pygame, sys, random
+import pygame, sys, random, time
 
 # Setup pygame/window ---------------------------------------- #
 mainClock = pygame.time.Clock()
 from pygame.locals import *
 
 pygame.init()
-pygame.display.set_caption('game base')
+pygame.display.set_caption('FOOTBALL')
 screen = pygame.display.set_mode((800, 600), 0, 32)
 
 font = pygame.font.SysFont(None, 20)
@@ -28,12 +28,9 @@ def main_menu():
 
         startbackground = pygame.image.load('start_bg.png')
         screen.blit(startbackground, (0, 0))
-
-
-        draw_text('Main Menu', font, (255, 255, 255), screen, 20, 20)
+        pygame.display.set_caption("FOOTBALL")
 
         mx, my = pygame.mouse.get_pos()
-
         button_1 = pygame.Rect(65, 50, 200, 78)
         button_2 = pygame.Rect(65, 150, 200, 78)
         if button_1.collidepoint((mx, my)):
@@ -63,6 +60,7 @@ def main_menu():
 
 def game():
     running = True
+    time.sleep(1)
     # making the screen
     screen = pygame.display.set_mode((800, 600))
 
@@ -131,6 +129,7 @@ def game():
                     # if user enters wrong answer
                     if userans.strip() not in (ans1) or userans.strip == "":
                         userans = "WRONG"
+                        p-=0.150
                     else:
                         # for printing new question after user enters right answer
                         userans = ""
@@ -164,7 +163,6 @@ def game():
 
         # making background appear
         screen.blit(background, (0, 0))
-
         # for making the football appear
         football(iconX, iconY)
 
@@ -192,10 +190,17 @@ def instructions():
     while running:
         screen.fill((0, 0, 0))
 
-        draw_text('''Instructions: This is basically a twist on the world famous game "Football". \n
-        Here the ball by default moves towards our goal (computer is winning) with a constant speed. \n
-        The user needs to solve simple addition problems to slow down the ball and eventually make it move towards the left
-         (into the computer's goal).''', font, (255, 255, 255), screen, 20, 20)
+        draw_text("Instructions: ", font, (255, 255, 255), screen, 20, 20)
+        draw_text("i) This is basically a twist on the world famous game \"Football\".", font, (255, 255, 255), screen, 20, 60)
+        draw_text("ii) Here the ball by default moves towards our goal (computer is winning) with a constant speed.", font, (255, 255, 255), screen, 20, 100)
+        draw_text("iii) The user needs to solve simple arithmetic problems to slow down the ball and towards the computer's goal.", font, (255, 255, 255), screen, 20, 140)
+        draw_text("iv) Problems will be flashed on the screen and the user needs to solve them as fast as possible.", font, (255, 255, 255), screen, 20, 180)
+        draw_text("v) The user has to type the answer and then press space bar.", font, (255, 255, 255), screen, 20, 220)
+        draw_text("vi) If the answer is correct the rate of going right of the ball will get reduced and eventually the ball will move left.", font, (255, 255, 255), screen, 20, 260)
+        draw_text("vii) If the answer is wrong then FALSE will be printed which the user has to manually remove by pressing backspace.", font, (255, 255, 255), screen, 20, 300)
+        draw_text("viii) The more the user solves questions correctly , faster will the ball move left (into computer's goal).", font, (255, 255, 255), screen, 20, 340)
+        draw_text("Pro tip: Press esc to go to main menu.", font, (255, 255, 255), screen, 400, 550)
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -206,6 +211,5 @@ def instructions():
 
         pygame.display.update()
         mainClock.tick(60)
-
 
 main_menu()
