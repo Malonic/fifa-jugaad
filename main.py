07812faa74e58c6e040b0b09a090f237a62ef2f9
Aditya <adityabanka08@gmail.com>
2022-01-17 23:29:30 +0530

    added credits

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ pygame.init()
 pygame.display.set_caption('FOOTBALL')
 screen = pygame.display.set_mode((800, 600), 0, 32)
 mixer.music.load('start_music.mp3')
+mixer.music.set_volume(0.2)
 mixer.music.play()
 
 font = pygame.font.SysFont(None, 20)
@@ -36,12 +37,17 @@ def main_menu():
         mx, my = pygame.mouse.get_pos()
         button_1 = pygame.Rect(65, 50, 200, 78)
         button_2 = pygame.Rect(65, 150, 200, 78)
+        button_3 = pygame.Rect(65, 260, 200, 78)
+
         if button_1.collidepoint((mx, my)):
             if click:
                 game()
         if button_2.collidepoint((mx, my)):
             if click:
                 instructions()
+        if button_3.collidepoint((mx,my)):
+            if click:
+                credits()
 
 
         click = False
@@ -67,6 +73,7 @@ def game():
     # making the screen
     screen = pygame.display.set_mode((800, 600))
     mixer.music.load('football_music.mp3')
+    mixer.music.set_volume(0.2)
     mixer.music.play()
 
     # for setting font
@@ -160,8 +167,9 @@ def game():
                         '''for increasing the value of 'p' (variable which dcides the amount by which ball 
                         moves left / the speed of going right decreases when user enters correct answer)'''
                 if event.key == K_ESCAPE:
-                    mixer.music.stop()
+                    mixer.music.fadeout(50)
                     mixer.music.load('start_music.mp3')
+                    mixer.music.set_volume(0.2)
                     mixer.music.play()
                     running = False
 
@@ -187,14 +195,18 @@ def game():
         elif iconX < 0:
             hello = base_font.render("YOU WON", True, (0, 0, 0))
             screen.blit(hello, (0, 20))
-            mixer.music.stop()
+            draw_text("Pro tip: Press esc to go to main menu.", font, (0, 0, 0), screen, 10, 80)
+            mixer.music.fadeout(50)
             mixer.music.load('start_music.mp3')
+            mixer.music.set_volume(0.2)
             mixer.music.play()
         else:
             bye = base_font.render("YOU LOST", True, (0, 0, 0))
             screen.blit(bye, (0, 20))
-            mixer.music.stop()
+            draw_text("Pro tip: Press esc to go to main menu.", font, (0, 0, 0), screen, 10, 80)
+            mixer.music.fadeout(50)
             mixer.music.load('start_music.mp3')
+            mixer.music.set_volume(0.2)
             mixer.music.play()
 
         pygame.display.update()
@@ -206,17 +218,38 @@ def instructions():
     while running:
         screen.fill((0, 0, 0))
 
-        draw_text("Instructions: ", font, (255, 255, 255), screen, 20, 20)
-        draw_text("I) This is basically a twist on the world famous game \"Football\".", font, (255, 255, 255), screen, 20, 60)
-        draw_text("II) Here the ball by default moves towards our goal (computer is winning) with a constant speed.", font, (255, 255, 255), screen, 20, 100)
-        draw_text("III) The user needs to solve simple arithmetic problems to slow down the ball and towards the computer's goal.", font, (255, 255, 255), screen, 20, 140)
-        draw_text("IV) Problems will be flashed on the screen and the user needs to solve them as fast as possible.", font, (255, 255, 255), screen, 20, 180)
-        draw_text("V) The user has to type the answer and then press space bar.", font, (255, 255, 255), screen, 20, 220)
-        draw_text("VI) If the answer is correct the rate of going right of the ball will get reduced and eventually the ball will move left.", font, (255, 255, 255), screen, 20, 260)
-        draw_text("VII) If the answer is wrong then FALSE will be printed which the user has to manually remove by pressing backspace.", font, (255, 255, 255), screen, 20, 300)
-        draw_text("VIII) The more the user solves questions correctly , faster will the ball move left (into computer's goal).", font, (255, 255, 255), screen, 20, 340)
-        draw_text("IX) If the user gets a question wrong, the speed of movement towards left gets slower.", font, (255, 255, 255), screen, 20, 380)
-        draw_text("Pro tip: Press esc to go to main menu.", font, (255, 255, 255), screen, 400, 550)
+        draw_text("Instructions: ", font, (255, 0, 0), screen, 20, 20)
+        draw_text("I) This is basically a twist on the world famous game \"Football\".", font, (255, 0, 0), screen, 20, 60)
+        draw_text("II) Here the ball by default moves towards our goal (computer is winning) with a constant speed.", font, (255, 0, 0), screen, 20, 100)
+        draw_text("III) The user needs to solve simple arithmetic problems to slow down the ball and towards the computer's goal.", font, (255, 0, 0), screen, 20, 140)
+        draw_text("IV) Problems will be flashed on the screen and the user needs to solve them as fast as possible.", font, (255, 0, 0), screen, 20, 180)
+        draw_text("V) The user has to type the answer and then press space bar.", font, (255, 0, 0), screen, 20, 220)
+        draw_text("VI) If the answer is correct the rate of going right of the ball will get reduced and eventually the ball will move left.", font, (255, 0, 0), screen, 20, 260)
+        draw_text("VII) If the answer is wrong then FALSE will be printed which the user has to manually remove by pressing backspace.", font, (255, 0, 0), screen, 20, 300)
+        draw_text("VIII) The more the user solves questions correctly , faster will the ball move left (into computer's goal).", font, (255, 0, 0), screen, 20, 340)
+        draw_text("IX) If the user gets a question wrong, the speed of movement towards left gets slower.", font, (255, 0, 0), screen, 20, 380)
+        draw_text("Pro tip: Press esc to go to main menu.", font, (255, 0, 0), screen, 400, 550)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+def credits():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+
+        draw_text("Credits: ", font, (255, 0, 0), screen, 20, 20)
+        draw_text("This game was made by Aditya Banka and Archit Choudhary of class XI- G. ", font, (255, 0, 0), screen, 20, 60)
+        draw_text("It took us a lot of time to make this so please give us a good rating if you are able to see this!", font, (255, 0, 0), screen, 20, 100)
+        draw_text("Pro tip: Press esc to go to main menu.", font, (255, 0, 0), screen, 400, 550)
 
         for event in pygame.event.get():
             if event.type == QUIT:
